b59901e1ac69235c10188c26b41b74142d6e73e3
vycb <vycb777@gmail.com>
2018-11-03 08:00:58 +0200

    v2.2 Support for new styles added to work with facebook

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
 var IITarget = {},
-		st = "; filter:invert(1);";
+		st = "; filter:invert(1);"
 
 browser.runtime.onMessage.addListener(function(msg){
 	if (msg.cmd !== "invert") return
 
-	var node = IITarget,am=[],tcus=[],medimg=[],mis=[],uisc=[],_5dec=[],
-	_2a2q=[],rn1oszu61=[],done=false;
-	_2a2q = node.getElementsByClassName("_2a2q");
+	var node = IITarget,am=[],medimg=[],mis=[],done=false;
 	if(node.parentNode.parentNode &&
 			node.parentNode.parentNode.parentNode &&
 			node.parentNode.parentNode.parentNode.parentNode &&
 			node.parentNode.parentNode.parentNode.parentNode.parentNode){
-				if(_2a2q.length ==0){
-				_2a2q = node.parentNode.parentNode.getElementsByClassName("_2a2q");
-				}
-		rn1oszu61 = node.parentNode.parentNode.getElementsByClassName("rn-1lgpqti");
-		medimg = node.parentNode.parentNode.getElementsByClassName("media-image");
-		uisc = node.parentNode.parentNode.getElementsByClassName("uiScaledImageContainer");
-		tcus = node.parentNode.parentNode.getElementsByClassName("tcu-imageWrapper");
-		mis = node.parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByClassName("_3x-2");
-
-		if(node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode &&
+		for(let cl of ["_2a2q", "media-image", "_2rea", "_5rgt", "_5dec", "rn-1lgpqti, uiScaledImageContainer, tcu-imageWrapper"]){
+			medimg = node.parentNode.parentNode.getElementsByClassName(cl);
+			if(medimg.length > 0) break;
+		}
+		if(medimg.length == 0){
+			for(let cl of ["_5rgu", "_3x-2"]){
+				mis = node.parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByClassName(cl);
+				if(mis.length > 0) break;
+			}
+		}else if(node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode &&
 				node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode){
 				am = node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByClassName("AdaptiveMedia");
 		}
 	}
 
-	if(tcus.length == 0 && mis.length == 0 && medimg.length == 0 && am.length == 0
-	&& uisc.length == 0 && _5dec.length == 0 && _2a2q.length == 0 &&rn1oszu61.length==0){
+	if(mis.length == 0 && medimg.length == 0 && am.length == 0){
 		nodechk(node)
 	}
-	else
-	{
-		for(let el of [am, mis, medimg, tcus, uisc,_2a2q,_5dec,rn1oszu61]){
+	else{
+		var done=false;
+		for(let el of [am, mis, medimg]){
 			Array.prototype.filter.call(el, ob=>{
-				nodechk(ob)
-				done=true
+				nodechk(ob);
+				done=true;
 			})
 			if(done)
-					break
+					break;
 		}
 	}
 });
 
-function nodechk(node){
-	var sty=node.getAttribute("style")
+function nodechk(ob){
+	var sty=ob.getAttribute("style")
+	if(!sty){
+		sty=''
+	}
 	if(sty && sty.indexOf(st) >-1){
 		sty=sty.replace(st, '')
-		node.setAttribute("style",sty)
 	}else{
-		node.setAttribute("style", sty + st);
+		sty = sty+st;
 	}
+	ob.setAttribute("style", sty)
 }
 
 addEventListener('contextmenu', function (e) {
