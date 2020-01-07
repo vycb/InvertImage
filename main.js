cbfca9a9590f561f00afc89984b356bd4affade4
vycb <vycb777@gmail.com>
2020-01-07 13:29:56 +0200

    v2.6 hue-rotate(180deg) Feature by utopianknight

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-var IITarget = {},
-		st = "; filter:invert(1);"
+var IITarget = {}
+const	ST = ";filter:invert(1) hue-rotate(180deg);",
+	/* filter:invert(1) hue-rotate(180deg); Feature: The colors only change from light to dark and vice versa {{{
+	 By utopianknight (https://github.com/utopianknight) }}}*/
+	SCLS=["r-1p0dtai","css-1dbjc4n", "rn-156q2ks","rn-10m9thr",
+	"rn-16y2uox","_26ii _-_b", "_2a2q", "media-image", "_2rea", "_5rgt",
+	"_5dec", "uiScaledImageContainer", "tcu-imageWrapper","_5rgu","_3x-2",
+	"AdaptiveMedia"],
+	SIDS=["AdaptiveMedia"]
 
 browser.runtime.onMessage.addListener(function(msg){
 	if (msg.cmd !== "invert") return
 
 	var node = IITarget, found=false, medimg=[],	pref=".";
-	const	scls=["css-1dbjc4n","rn-156q2ks","rn-16y2uox","rn-10m9thr","_26ii _-_b", "_2a2q", "media-image", "_2rea", "_5rgt", "_5dec", "uiScaledImageContainer", "tcu-imageWrapper","_5rgu", "_3x-2","AdaptiveMedia"]
-	const	sids=["AdaptiveMedia"]
-	for(let cl of scls){
+	for(let cl of SCLS){
 		//medimg = node.parentNode.parentNode.getElementsByClassName(cl);
-		if(sids.indexOf(cl)>-1) pref="#"
+		if(SIDS.indexOf(cl)>-1) pref="#"
 		medimg = node.closest(pref+cl)
 		//medimg = findAncestor(node, cl);
 		if(medimg){
@@ -29,14 +34,14 @@ function nodechk(ob){
 	if(!sty){
 		sty=''
 	}
-	if(sty && sty.indexOf(st) >-1){
-		sty=sty.replace(st, '')
+	if(sty && sty.indexOf(ST) >-1){
+		sty=sty.replace(ST, '')
 	}else{
-		sty = sty+st;
+		sty = sty+ST;
 	}
 	ob.setAttribute("style", sty)
 }
 
-addEventListener('contextmenu', function (e) {
+addEventListener('contextmenu', e => {
 	IITarget = e.target
 }, false);
